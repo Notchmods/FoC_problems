2e0cdd2301d8dc09f80bfbb8db4f2fe721132f66
Notchmods <43086357+Notchmods@users.noreply.github.com>
2024-05-27 21:32:15 +1000

    Update file_IO.py
    CSV

--- file_IO.py
+++ file_IO.py
@@ -1,4 +1,4 @@
-"""Read files, (same thing for CSV)""""
+"""Read files""""
 def read_files("song.txt"):
         """r: read the file"""
     with open("song.txt","r") as songs:
@@ -20,3 +20,15 @@ def write_files("songs.txt"):
     writing.close()
     return
 
+#Reading CSV files
+"""Same thing as reading and writing files except
+you need to specify reader or writer"""
+import csv
+def csvplay(csvfiles):
+    with open(csvfiles,"r") as files:
+            #This
+        csv_reader=csv.reader(files)
+        for lines in csv_reader:
+            print(lines)
+    files.close()
+    return
