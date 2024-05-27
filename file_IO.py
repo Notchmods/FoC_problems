562736bde44fe3c153cba22ebbccec88f42df080
Notchmods <43086357+Notchmods@users.noreply.github.com>
2024-05-27 21:25:24 +1000

    Update file_IO.py
    Added writing feature

--- file_IO.py
+++ file_IO.py
@@ -1,8 +1,22 @@
 """Read files""""
-def approximate_song("song.txt"):
-    with open("fakesong.txt","r") as songs:
+def read_files("song.txt"):
+        """r: read the file"""
+    with open("song.txt","r") as songs:
         for lines in songs.read().split("\n"):
             print(lines)
     
     songs.close()
     return
+
+
+"""Write files"""
+def write_files("songs.txt"):   
+    """a: Append things into the file without erasing
+        c: Erase everything and write into file"""
+    with open("song.txt","a") as writing:
+        writing.write("Let me sing a song ") 
+    
+    
+    writing.close()
+    return
+
