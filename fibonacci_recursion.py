420b937a61b40bd4da0174e0d26ac152b7e37408
Notchmods <43086357+Notchmods@users.noreply.github.com>
2024-08-08 10:51:35 +1000

    Update fibonacci_recursion.py
    Showing example of fibonacci sequence

--- fibonacci_recursion.py
+++ fibonacci_recursion.py
@@ -2,14 +2,14 @@ def fibonacci(n):
     """Add the previous number and the previous 2 number to form
     a new number"""
     #Backwards addition until it hits base case
-    ""E.g. fibonacci(4)
+    """E.g. fibonacci(4)
     4- 3+2=5
     3-2+1=3
     2-1+1=2
     1-1
 
     1+1=2+1=3+2=5
-    ""
+    """
     if n==0:
         return 0
     elif n==1:
